f9dc63d4634efa5ca20deba6ec3499aa10cfe7ac
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-06-02 20:59:52 -0400

    working api

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 Savage': {
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+
+    'chance the Rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': ' Illinois, U.S'
+    },
+
+    'eminem': {
+        'age': 49,
+        'birthName': 'Marshall Bruce Mathers III',
+        'birthLocation': 'Missouri, U.S'
+    },
+
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+    if (rappers[rapperName]) {
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers['unknown'])
+    }
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`I'm at port${PORT}`)
+})
