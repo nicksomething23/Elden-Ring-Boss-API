8638f17015be3ffdb50840a9efc422a093ca7a56
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-06-04 17:22:00 -0400

    update api to provide elden ring boss information

--- server.js
+++ server.js
@@ -6,30 +6,35 @@ const PORT = 8000
 
 app.use(cors())
 
-const rappers = {
-    '21 Savage': {
-        'age': 29,
-        'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England'
-    },
-
-    'chance the Rapper': {
-        'age': 29,
-        'birthName': 'Chancelor Bennett',
-        'birthLocation': ' Illinois, U.S'
-    },
-
-    'eminem': {
-        'age': 49,
-        'birthName': 'Marshall Bruce Mathers III',
-        'birthLocation': 'Missouri, U.S'
+const eldenRingMainBosses = {
+    'godrick the grafted': {
+        'location': 'Stormveil Castle',
+        'health': 6080,
+        'runes': 20000,
+        'drops': ["Godrick's Great Rune",'Remembrance of the Grafted']
     },
 
     'unknown': {
-        'age': 0,
-        'birthName': 'unknown',
-        'birthLocation': 'unknown'
-    }
+        'location': 'lands between',
+        'health': 0,
+        'runes': 0,
+        'drops': 'unknown'
+    }, 
+    
+    'rennala, queen of the full moon': {
+        'location': 'Raya Lucaria Academy',
+        'health': ["phase one: 3493", "phase one: 4097"],
+        'runes': 40000,
+        'drops': ["Remembrance of the Full Moon Queen",'Great Rune of the Unborn']
+    }, 
+    
+    'starscourge radahn': {
+        'location': 'Redmane Castle, Caelid',
+        'health':  9572,
+        'runes': 70000,
+        'drops': ["Remembrance of the Starscourge","Radahn's Great Rune"]
+    }, 
+    
 }
 
 app.get('/', (request, response) => {
@@ -37,11 +42,11 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/:name', (request, response) => {
-    const rapperName = request.params.name.toLowerCase()
-    if (rappers[rapperName]) {
-        response.json(rappers[rapperName])
+    const bossName = request.params.name.toLowerCase()
+    if (eldenRingMainBosses[bossName]) {
+        response.json(eldenRingMainBosses[bossName])
     } else {
-        response.json(rappers['unknown'])
+        response.json(eldenRingMainBosses['unknown'])
     }
     
 })
